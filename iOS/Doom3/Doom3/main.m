10d9062b6dc73314d8dc1f2af2481433eeab6865
Chinmay Garde <chinmay@apportable.com>
2014-03-15 21:41:58 -0700

    Initial project setup

---
+++ iOS/Doom3/Doom3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Doom3
+//
+//  Created by Chinmay Garde on 3/15/14.
+//  Copyright (c) 2014 Chinmay Garde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
